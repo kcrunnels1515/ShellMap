fcab700f4751e9c43d24c5fe74c505c0b98d61ab
Kelly Runnels <kcrunnels15@gmail.com>
2025-02-19 16:13:47 -0500

    Added argument parsing

--- server/server.py
+++ server/server.py
@@ -5,6 +5,12 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from socketserver import *
 from urllib.parse import urlparse
 import datetime
+import custom_optparse as co
+from optparse import OptionParser
+import os
+
+# load module file name
+modular_files = [ f.path for f in os.scandir(os.path.join(os.getcwd(), "modules")) if f.is_file() ]
 
 def encode(data: str) -> str:
     # key for XOR encoding is current minute
@@ -35,33 +41,41 @@ def decode(data: str) -> str:
     # convert hex string to ascii string
     return bytearray.fromhex(''.join(decoded)).decode()
 
-'''
-Specification of arguments to nmap
-
-host_disc ::= '-'s[LnSTAU]
-
-options ::= '-'(p' '?port_list | sV)
-
-port_list ::= port_elem | port_elem (',' | ' '+',') port_list
-port_elem ::= port | port '-' port
-port ::= [1-65535]
-
-'''
-
 class Argument:
-    def __init__(self, arg_str):
-        self.arg_strs = arg_str.split(' ')
-        self.ind = 0
+    def __init__(self):
         self.flags = []
         self.ports = []
         self.addrs = dict()
+        self.regexes = dict()
+        self.parser = OptionParser(option_class=ShellMapOption)
+        self.load_parser_rules()
 
-#    def parse_args(self):
+    def load_parser_rules(self):
+        self.parser.add_option("-p", action="store", type="port_list", dest="ports")
+        self.parser.add_option("-sL", action="store_true", dest="list_scan", default=False)
+        self.parser.add_option("-sn", action="store_true", dest="ping_scan", default=False)
+        self.parser.add_option("-Pn", action="store_false", dest="host_disc", default=True)
+        self.parser.add_option("-PS", action="store", type="port_list", dest="port_syn")
+        self.parser.add_option("-PA", action="store", type="port_list", dest="port_ack")
+        self.parser.add_option("-PU", action="store", type="port_list", dest="port_udp")
+        self.parser.add_option("-PY", action="store", type="port_list", dest="port_sctp")
+        self.parser.add_option("-PE", action="store_true", dest="icmp_echo", default=False)
+        self.parser.add_option("-PP", action="store_true", dest="icmp_timestamp", default=False)
+        self.parser.add_option("-PM", action="store_true", dest="icmp_netmasq", default=False)
+        self.parser.add_option("-n", action="store_false", dest="no_resolv", default=True)
+        self.parser.add_option("-sS", action="store_const", const="SYN", dest="port_default_scan", default="SYN")
+        self.parser.add_option("-sT", action="store_const", const="CON", dest="port_default_scan", default="SYN")
+        self.parser.add_option("-sA", action="store_const", const="ACK", dest="port_default_scan", default="SYN")
+        self.parser.add_option("-sU", action="store_true", dest="port_udp_default", default=False)
+        self.parser.add_option("--exclude-ports", action="store", type="port_list", dest="excluded_ports")
+        self.parser.add_option("-F", action="store_true", dest="limit_ports", default=False)
+        self.parser.add_option("-r", action="store_false", dest="randomize_ports", default=True)
+        self.parser.add_option("--top-ports", action="store", type="int", dest="top_ports")
+        self.parser.add_option("-sV", action="store_true", dest="service_version", default=False)
+        self.parser.add_option("-O", action="store_true", dest="os_detect", default=False)
 
-
-    def add_flag(self, flag):
-        self.flags.add(flag)
-
+    def parse(self, arg_str):
+        return self.parser.parse_args(arg_str.split(' '))
 
 
 class Flag(Enum):
@@ -76,17 +90,39 @@ class Flag(Enum):
     ALL = 9
 
 class RequestHandler(BaseHTTPRequestHandler):
+    def __init___(self):
+        self.cur_arg = Argument()
+
+    def collect_script(self, arg_str):
+        return "test"
+
     def do_GET(self):
         query = urlparse(self.path).query
         query_components = dict(qc.split("=") for qc in query.split("&"))
-        args = query_components['args']
-
-        if len(args) > 0:
-            self.send_response(200)
+        if len(query_components) <= 0 or "args" not in query_components:
+            self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write()
+            self.wfile.write("no args\n")
+        else:
+            query_str = decode(query_components['args'])
+            if len(query_str) > 0:
+                self.send_response(200)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(encode(collect_script(query_str)))
+            else:
+                self.send_response(404)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write("no args\n")
 
 
-#if __name__ == "__main__":
-#    print(decode(encode("this is a test string")))
+def run_server(server_class=HTTPServer, handler_class=RequestHandler, port=8000):
+    server_addr = ('', port)
+    httpd = server_class(server_addr, handler_class)
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run_server()
