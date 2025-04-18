cd8976c07c51c8762dabdfbb5a631ea52d77420e
Kelly Runnels <kcrunnels15@gmail.com>
2025-04-18 16:19:36 -0400

    more QOL changes to run.sh

--- run.sh
+++ run.sh
@@ -14,16 +14,6 @@ SESSION="shellmap_server"
 CONNECT_SESSION="y"
 
 case "$#" in
-	0)
-		if which dig 2>/dev/null >/dev/null; then
-			echo "Looking up public IP address using dig..."
-			ip_addr=$(dig myip.opendns.com @resolver1.opendns.com +short | grep -v ";;")
-			echo "Public IP address found: $IP_ADDR"
-		else
-			echo "Please install dig or provide a public IP address"
-			exit
-		fi
-		;;
 	1)
 		if [ "$1" = "--help" ]; then
 			echo "Syntax: ./run.sh [ip address or domain name]"
